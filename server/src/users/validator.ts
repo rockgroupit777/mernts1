20224886963e0c2de61355041cc2a6cb0b01b4f4
rock <rockgroupit777@gmail.com>
2022-12-21 21:03:46 +0700

    first

--- server/src/users/validator.ts
+++ server/src/users/validator.ts
@@ -1,29 +1,44 @@
 import Joi from 'joi';
-export const userSchema = Joi.object({
-  username: Joi.string().alphanum().min(2).max(50).trim().required().label("UserName"),
-  email: Joi.string()
+
+  const username= Joi.string().alphanum().min(2).max(50).trim().required().label("UserName");
+  const email= Joi.string()
   .email({ minDomainSegments: 2 })
   .trim()
   .required()
-  .label("Email"),
-  password: Joi.string()
+  .label("Email");
+  const password= Joi.string()
   .pattern(new RegExp('^[a-zA-Z0-9]{8,100}$'))
   .message(
     "password must have at least one uppercase letter, one lowercase letter and one digit"
   )
   .required()
-  .label("Password"),
-  repeatPassword: Joi.ref("password"),
-  firstName: Joi.string().max(50).trim().label("FirstName"),
-  lastName: Joi.string().max(50).trim().label("LastName"),
-  access_token: [
+  .label("Password");
+  const repeatPassword= Joi.ref("password");
+  const firstName= Joi.string().max(50).trim().label("FirstName");
+  const lastName = Joi.string().max(50).trim().label("LastName");
+  const access_token= [
     Joi.string(),
     Joi.number()
-  ],
-  avatar: Joi.string().label("Avatar")
+  ];
+  const avatar= Joi.string().label("Avatar");
+
+export const signUpValidate = Joi.object().keys({
+  username,
+  email,
+  password,
+  repeatPassword,
+  firstName,
+  lastName,
+  avatar
 })
-  .with('email','password')
-  .xor('password','access_token')
-  .with('password','repeatPassword');
-
-
+export const signInValidate = Joi.object().keys({
+  email,
+  password
+})
+export const updateUserValidate = Joi.object().keys({
+  password,
+  repeatPassword,
+  firstName,
+  lastName,
+  avatar
+})
